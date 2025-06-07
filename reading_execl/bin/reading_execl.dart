6e12f0c3c5623283e7849f422b1483dc1c10e83c
YabseraBogale <yabserapython@gmail.com>
2025-06-07 17:41:17 +0300

    ok

---
+++ reading_execl/bin/reading_execl.dart
@@ -0,0 +1,5 @@
+import 'package:reading_execl/reading_execl.dart' as reading_execl;
+
+void main(List<String> arguments) {
+  print('Hello world: ${reading_execl.calculate()}!');
+}
---
+++ reading_execl/test/reading_execl_test.dart
@@ -0,0 +1,8 @@
+import 'package:reading_execl/reading_execl.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
