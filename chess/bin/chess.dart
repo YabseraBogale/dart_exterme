99b51d118bbb67a3915a6ce5c26165010a6a2fe4
YabseraBogale <yabserapython@gmail.com>
2025-06-08 12:04:14 +0300

    ok

--- chess/bin/chess.dart
+++ chess/bin/chess.dart
@@ -1,5 +1,11 @@
-import 'package:chess/chess.dart' as chess;
+import 'package:shelf/shelf_io.dart' as io;
+import 'package:shelf_static/shelf_static.dart';
 
-void main(List<String> arguments) {
-  print('Hello world: ${chess.calculate()}!');
+Future<void> main(List<String> arguments) async {
+  var handler = createStaticHandler('web', defaultDocument: 'index.html');
+
+  var server = await io.serve(handler, 'localhost', 8080);
+  print(
+    'Static file server running at http://${server.address.host}:${server.port}',
+  );
 }
