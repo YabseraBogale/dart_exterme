b2bc3c4cfe5a9b64831214c83e9d0a737730bf0c
YabseraBogale <yabserapython@gmail.com>
2025-06-10 19:44:23 +0300

    ok

---
+++ dart_h/bin/dart_h.dart
@@ -0,0 +1,5 @@
+import 'package:dart_h/dart_h.dart' as dart_h;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_h.calculate()}!');
+}
---
+++ dart_h/test/dart_h_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_h/dart_h.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
