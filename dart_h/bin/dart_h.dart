a259a98889b1d3bcabf031b721e9a88ae6f3e840
YabseraBogale <yabserapython@gmail.com>
2025-06-10 20:08:09 +0300

    ok

--- dart_h/bin/dart_h.dart
+++ dart_h/bin/dart_h.dart
@@ -1,5 +1,15 @@
-import 'package:dart_h/dart_h.dart' as dart_h;
+import 'package:html/parser.dart';
+import 'package:http/http.dart' as http;
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_h.calculate()}!');
+Future<void> main(List<String> arguments) async {
+  // hentai force .container-xl
+  var uri = Uri.parse("https://hentaiforce.net/view/203032");
+  var data = await http.get(uri);
+  if (data.statusCode == 200) {
+    var dom = parse(data.body);
+    var result = dom.querySelectorAll("lazy small-bg-load entered loaded");
+    print(result.length);
+  } else {
+    print('Failed to load data, status code: ${data.statusCode}');
+  }
 }
