f2d12c9c1fc571f16ecb6cebd0a98c2a53ca4bc5
YabseraBogale <yabserapython@gmail.com>
2025-06-22 12:17:51 +0300

    ok

--- hot_reload/bin/hot_reload.dart
+++ hot_reload/bin/hot_reload.dart
@@ -1,3 +1,9 @@
 import 'dart:io';
 
-void main(List<String> arguments) async {}
+void main(List<String> arguments) async {
+  var status = await Process.run("git", ["status"]);
+  var test = status.stdout.toString().split("\n")[3];
+  if (test == "Changes not staged for commit:") {
+    print("ok");
+  } else if (test == "") {}
+}
