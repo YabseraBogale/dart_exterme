4fa8d12d24a8c46d097750622dc1afde87025f8b
YabseraBogale <yabserapython@gmail.com>
2025-06-22 11:36:33 +0300

    ok

--- hot_reload/bin/hot_reload.dart
+++ hot_reload/bin/hot_reload.dart
@@ -1,3 +1,5 @@
 import 'package:hot_reload/hot_reload.dart' as hot_reload;
 
-void main(List<String> arguments) {}
+void main(List<String> arguments) {
+  print(hot_reload.Reload());
+}
--- hot_reload/lib/hot_reload.dart
+++ hot_reload/lib/hot_reload.dart
@@ -1,1 +1,6 @@
 import 'dart:io';
+
+dynamic Reload() async {
+  var status = await Process.run("git", ["status"]);
+  return status.stdout();
+}
