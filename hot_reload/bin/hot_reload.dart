2fc05291229d6e8843c961a3d5b94170c7c26ba1
YabseraBogale <yabserapython@gmail.com>
2025-06-22 11:32:22 +0300

    ok

--- hot_reload/bin/hot_reload.dart
+++ hot_reload/bin/hot_reload.dart
@@ -1,5 +1,3 @@
 import 'package:hot_reload/hot_reload.dart' as hot_reload;
 
-void main(List<String> arguments) {
-  print('Hello world: ${hot_reload.calculate()}!');
-}
+void main(List<String> arguments) {}
