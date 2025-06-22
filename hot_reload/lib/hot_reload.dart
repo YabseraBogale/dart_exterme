d81249a1fd8ea75f4ea7fd218b48d06e0155936e
YabseraBogale <yabserapython@gmail.com>
2025-06-22 12:31:07 +0300

    ok

--- hot_reload/lib/hot_reload.dart
+++ hot_reload/lib/hot_reload.dart
@@ -1,22 +1,3 @@
 import 'dart:io';
 
-Future<void> server() async {
-  try {
-    final serve = await HttpServer.bind("localhost", 8080);
-    await for (HttpRequest request in serve) {
-      print("server started in localhost:8080");
-      if (request.uri.path == "/") {
-        try {
-          var status = await Process.run("git", ["status"]);
-          var result = status.stdout.toString().split("\n")[3];
-          if (result == "Changes not staged for commit:") {
-          } else if (result == "nothing to commit, working tree clean") {}
-        } catch (e) {
-          print(e);
-        }
-      }
-    }
-  } catch (e) {
-    print(e);
-  }
-}
+Future<void> server() async {}
