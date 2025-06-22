16e106566ad139571cb44fdced916ed9b2710889
YabseraBogale <yabserapython@gmail.com>
2025-06-22 11:16:42 +0300

    ok

---
+++ hot_reload/bin/hot_reload.dart
@@ -0,0 +1,5 @@
+import 'package:hot_reload/hot_reload.dart' as hot_reload;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hot_reload.calculate()}!');
+}
---
+++ hot_reload/test/hot_reload_test.dart
@@ -0,0 +1,8 @@
+import 'package:hot_reload/hot_reload.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
