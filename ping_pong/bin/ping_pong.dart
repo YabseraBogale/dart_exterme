91c69e5e2465c96d7aef6bf55d817cac43ffd333
YabseraBogale <yabserapython@gmail.com>
2025-06-24 20:34:31 +0300

    ok

---
+++ ping_pong/bin/ping_pong.dart
@@ -0,0 +1,5 @@
+import 'package:ping_pong/ping_pong.dart' as ping_pong;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ping_pong.calculate()}!');
+}
---
+++ ping_pong/test/ping_pong_test.dart
@@ -0,0 +1,8 @@
+import 'package:ping_pong/ping_pong.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
