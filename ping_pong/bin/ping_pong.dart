326e3e7a0ab5932608da4d3c9f7de633d680fe36
YabseraBogale <yabserapython@gmail.com>
2025-06-25 19:40:05 +0300

    ok

--- ping_pong/bin/ping_pong.dart
+++ ping_pong/bin/ping_pong.dart
@@ -1,1 +1,11 @@
-void main(List<String> arguments) {}
+import 'dart:io';
+
+void main(List<String> arguments) async {
+  try {
+    var server = await HttpServer.bind("localhost", 8080);
+    WebSocket socket;
+    await for (HttpRequest request in server) {}
+  } catch (e) {
+    print(e);
+  }
+}
