ed7a734b5f13092e67e1f0b811b285c182a7e065
YabseraBogale <yabserapython@gmail.com>
2025-06-25 20:09:53 +0300

    ok

--- ping_pong/bin/ping_pong.dart
+++ ping_pong/bin/ping_pong.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 Future<void> main(List<String> arguments) async {
   try {
     var server = await HttpServer.bind("localhost", 8080);
-    WebSocket socket;
     await for (HttpRequest request in server) {
       var path = request.uri.path;
       var index = File("index.html");
