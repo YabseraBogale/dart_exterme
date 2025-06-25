ece9e14f8b337b49a4f625a134c856ce80be0a50
YabseraBogale <yabserapython@gmail.com>
2025-06-25 19:34:51 +0300

    ok

--- ping_pong/bin/ping_pong.dart
+++ ping_pong/bin/ping_pong.dart
@@ -1,5 +1,1 @@
-import 'package:ping_pong/ping_pong.dart' as ping_pong;
-
-void main(List<String> arguments) {
-  print('Hello world: ${ping_pong.calculate()}!');
-}
+void main(List<String> arguments) {}
