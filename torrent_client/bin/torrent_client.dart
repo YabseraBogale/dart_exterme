b5e2758f64ec10191567c6e0c78e4ec3f6ffe1ee
YabseraBogale <yabserapython@gmail.com>
2025-06-29 12:42:45 +0300

    ok

---
+++ torrent_client/bin/torrent_client.dart
@@ -0,0 +1,5 @@
+import 'package:torrent_client/torrent_client.dart' as torrent_client;
+
+void main(List<String> arguments) {
+  print('Hello world: ${torrent_client.calculate()}!');
+}
---
+++ torrent_client/test/torrent_client_test.dart
@@ -0,0 +1,8 @@
+import 'package:torrent_client/torrent_client.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
