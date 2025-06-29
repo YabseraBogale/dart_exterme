238c2724f15cbb1079332bf1922112379ce5e17d
YabseraBogale <yabserapython@gmail.com>
2025-06-29 19:13:22 +0300

    ok

--- torrent_client/bin/torrent_client.dart
+++ torrent_client/bin/torrent_client.dart
@@ -6,5 +6,5 @@ void main(List<String> arguments) {
   var torrent = File(
     "5a982c32f9e5bb7939900c7ec896c17f3c2e1bca.torrent",
   ).readAsBytesSync();
-  var d = Torrent.open(torrent);
+  Torrent.open(torrent);
 }
