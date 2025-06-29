7515ec3c01313a1bcf33143e8c8ccfe285885cc7
YabseraBogale <yabserapython@gmail.com>
2025-06-29 16:55:28 +0300

    ok

--- torrent_client/lib/torrent_client.dart
+++ torrent_client/lib/torrent_client.dart
@@ -4,13 +4,13 @@ import 'package:b_encode_decode/b_encode_decode.dart' as bencode;
 
 /*
   BencodeInfo {
-    files List<dynamic>{
+    files List<dynamic>[
       Map<String, dynamic>{
         length int,
         md5sum Uint8List,
         path List<dynamic>
       }
-    },
+    ],
     name Uint8List,
     piece length int,
     pieces Uint8List,
@@ -22,11 +22,22 @@ import 'package:b_encode_decode/b_encode_decode.dart' as bencode;
 class BencodeInfo {}
 
 /* BencodeTorrent  {
-    announce-list List<dynamic>
+    announce-list List<dynamic>[
+      List<dynamic>[
+        Uint8List
+      ]
+    ]
     BencodeInfo info
   }
 */
 
 class BencodeTorrent {}
 
-void open(Uint8List byteStream) {}
+void open(Uint8List byteStream) {
+  var ben = bencode.decode(byteStream)["announce-list"];
+  for (var i in ben) {
+    for (var k in i) {
+      print(k.runtimeType);
+    }
+  }
+}
