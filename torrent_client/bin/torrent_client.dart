42c6b1ff770ed7f68bdd7fd98b74148050deaf78
YabseraBogale <yabserapython@gmail.com>
2025-06-29 12:51:41 +0300

    ok

--- torrent_client/bin/torrent_client.dart
+++ torrent_client/bin/torrent_client.dart
@@ -1,5 +1,1 @@
-import 'package:torrent_client/torrent_client.dart' as torrent_client;
-
-void main(List<String> arguments) {
-  print('Hello world: ${torrent_client.calculate()}!');
-}
+void main(List<String> arguments) {}
