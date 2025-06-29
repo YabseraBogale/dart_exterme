f2a6b6464ca48575e19bc96462e81b0c18ed515b
YabseraBogale <yabserapython@gmail.com>
2025-06-29 12:41:59 +0300

    ok

--- windows_10_vs_11/bin/windows_10_vs_11.dart
+++
@@ -1,5 +0,0 @@
-import 'package:windows_10_vs_11/windows_10_vs_11.dart' as windows_10_vs_11;
-
-void main(List<String> arguments) {
-  print('Hello world: ${windows_10_vs_11.calculate()}!');
-}
--- windows_10_vs_11/test/windows_10_vs_11_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:windows_10_vs_11/windows_10_vs_11.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
