fee1b0b832db142ba5288638c01fee322681f121
YabseraBogale <yabserapython@gmail.com>
2025-07-25 19:07:03 +0300

    going back

--- gdrp/bin/gdrp.dart
+++
@@ -1,5 +0,0 @@
-import 'package:gdrp/gdrp.dart' as gdrp;
-
-void main(List<String> arguments) {
-  print('Hello world: ${gdrp.calculate()}!');
-}
--- gdrp/test/gdrp_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:gdrp/gdrp.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
