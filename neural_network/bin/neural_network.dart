e859759f582f14b1d53e957dbb00665d90afa7ae
YabseraBogale <yabserapython@gmail.com>
2025-11-02 20:01:32 +0300

    going to write neural network from scrach

---
+++ neural_network/bin/neural_network.dart
@@ -0,0 +1,5 @@
+import 'package:neural_network/neural_network.dart' as neural_network;
+
+void main(List<String> arguments) {
+  print('Hello world: ${neural_network.calculate()}!');
+}
---
+++ neural_network/test/neural_network_test.dart
@@ -0,0 +1,8 @@
+import 'package:neural_network/neural_network.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
