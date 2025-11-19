7cc86dccc58fd23f61d827b8e46cb625dcf879f3
YabseraBogale <yabserapython@gmail.com>
2025-11-19 19:24:09 +0300

    working on virus

---
+++ virus/bin/virus.dart
@@ -0,0 +1,5 @@
+import 'package:virus/virus.dart' as virus;
+
+void main(List<String> arguments) {
+  print('Hello world: ${virus.calculate()}!');
+}
---
+++ virus/test/virus_test.dart
@@ -0,0 +1,8 @@
+import 'package:virus/virus.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
